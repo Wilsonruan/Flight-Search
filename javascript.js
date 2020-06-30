a526f0c87a358fc9b4b782998266e334c04fe020
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-30 19:03:43 -0400

    Search for location

--- javascript.js
+++ javascript.js
@@ -1,15 +1,31 @@
-function destination () {
+$(document).ready(function() {
+    airportInfoAPI ($('#to-location'))
+    airportInfoAPI ($('#from-location'))
 
-}
+    $('#button-swap').click(function () {
+        var toLocation = $('#to-location').val();
+        var fromLocation = $('#from-location').val();
+        $('#to-location').val(fromLocation);
+        $('#from-location').val(toLocation);
+    })
+})
 
-function calendar () {
+function airportInfoAPI (airportInfo) {
 
-}
-
-function travelers () {
-
-}
-
-function searchFlight () {
-
+    airportInfo.focusout(function () {
+        var airportLocation = airportInfo.val();
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://airport-info.p.rapidapi.com/airport?iata=" + airportLocation,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "airport-info.p.rapidapi.com",
+                "x-rapidapi-key": "b54c55a6edmsh6c049f7b3fa366fp145a1ajsna8fa9fa1eefb"
+            }
+        }
+        $.ajax(settings).done(function (response) {
+            airportInfo.val(response['name'])
+        });
+    });
 }

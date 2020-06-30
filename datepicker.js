630a533ae4013c1558cbc3e53204841776c77e32
Sushma Kaluva <kaluvasushma@gmail.com>
2020-06-29 23:06:48 -0400

    added date picker logic and html structure,css

--- datepicker.js
+++ datepicker.js
@@ -1,5 +1,16 @@
 $(document).ready(() => {
   $('#depart-from').datepicker({
     showAnim: 'slideDown',
+    minDate: "0D",
+    maxDate: "+2Y",
+
   });
+
+  $('#return-to').datepicker({
+    showAnim: 'slideDown',
+    minDate: "1D",
+    maxDate: "+2Y",
+
+  });
+
 });

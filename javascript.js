4a5b444e4f9557449ac5d1c7cbda1de344165a2e
Sushma Kaluva <kaluvasushma@gmail.com>
2020-07-10 00:20:57 -0400

    Flight display details

--- javascript.js
+++ javascript.js
@@ -38,7 +38,7 @@ $(document).ready(() => {
   airportInfoAPI(fromAirport, $('#from-location-code'));
 
 
-    $('#button-swap').click(() => {
+  $('#button-swap').click(() => {
     const toLocation = toAirport.val();
     const fromLocation = fromAirport.val();
     const toLocationCode = $('#to-location-code').val();
@@ -48,4 +48,42 @@ $(document).ready(() => {
     $('#to-location-code').val(fromLocationCode)
     $('#from-location-code').val(toLocationCode)
   });
+
+  calcDistance('DEL', 'YYZ');
 });
+
+
+
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+    + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+    * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+}
+
+// calculate distance between src, destination using lat,long
+function calcDistance(sourceCode, destinationCode) {
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: `https://airport-info.p.rapidapi.com/airport?iata=${sourceCode}`,
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'airport-info.p.rapidapi.com',
+      'x-rapidapi-key': 'b54c55a6edmsh6c049f7b3fa366fp145a1ajsna8fa9fa1eefb',
+    },
+  };
+  $.ajax(settings).done((response) => {
+    console.log(response);
+  });
+}
